3e0f851e0a88937ce84f79d0efe54736c5ed7fa0
Deyvis <deyvisjaime17@gmail.com>
2021-04-09 20:57:58 -0500

    agregando vista responsive a estado de pandemia

---
+++ src/components/Molecules/RankingRegion.jsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react'
+
+const RankingRegional = () =>{
+    return(
+          <Fragment>
+              <div className="ranking">
+                    <div className="ranking-c">
+                        <h2>Ranking de impacto del COVID 19 por regiones</h2>
+                        <div className="list-region">
+                            <ul>
+                                <li>1. <span></span> Lima Región</li>
+                                <li>2. <span></span> Arequipa</li>
+                                <li>3. <span></span> Junín</li>
+                                <li>4. <span></span> Loreto</li>
+                                <li>5. <span></span> Ancash</li>
+                                <li>6. <span></span> Cusco</li>
+                                <li>7. <span></span> Ucayali</li>
+                            </ul>
+                        </div>
+                    </div>
+                   
+                </div>
+          </Fragment>
+    )
+}
+export default RankingRegional
