5c0bd74d75c053ac4fcddf9d31be893e27375710
quispewilmer <wilmerquispecode@gmail.com>
2021-04-20 22:00:59 -0500

    Set the element's position

--- src/components/Molecules/SelectPointOnMapBox.jsx
+++ src/components/Molecules/SelectPointOnMapBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import SelectPointOnMap from '../Atoms/SelectPointOnMap';
 import covidbed from '../../img/emergencymap/covidbed.svg';
 import essaludhealthcenter from '../../img/emergencymap/essaludhealthcenter.svg';
@@ -8,10 +8,53 @@ import oxigencharge from '../../img/emergencymap/oxigencharge.svg';
 import pharmacy from '../../img/emergencymap/pharmacy.svg';
 import privatehealthcenter from '../../img/emergencymap/privatehealthcenter.svg';
 import ucibed from '../../img/emergencymap/ucibed.svg';
+import arrowup from '../../img/icons/arrowup.svg';
 
 const SelectPointOnMapBox = ({theme}) => {
+    let updownButton, mapBoxGrid, mapBox, arrowIcon;
+
+    let [state, setState] = useState({
+        mapBoxGridIsDown: true,
+        mapBoxTitle: "Ocultar"
+    });
+
+    useEffect(() => {
+        updownButton = document.querySelector(".select-point-on-map-box__updown");
+        mapBoxGrid = document.querySelector(".select-point-on-map-box__grid");
+        mapBox = document.querySelector(".select-point-on-map-box");
+        arrowIcon = document.querySelector(".select-point-on-map-box__updownicon");
+    })
+
+    const getDownTheArea = () => {
+
+        setState({
+            mapBoxGridIsDown: state.mapBoxGridIsDown ? false : true,
+            mapBoxTitle: state.mapBoxTitle == "Ocultar" ? 'Ver más' : "Ocultar"
+        })
+        
+        moveTheElement();
+    }
+    
+    const moveTheElement = () => {
+        if(state.mapBoxGridIsDown) {
+            let bottomUpdownButton = updownButton.getBoundingClientRect().bottom;
+            let bottomMapBoxGrid = mapBoxGrid.getBoundingClientRect().bottom;
+            let difference = bottomMapBoxGrid - bottomUpdownButton;
+            
+            mapBox.style.transform = `translateY(${difference}px)`;
+            arrowIcon.style.transform = `rotate(180deg)`;
+        } else {
+            mapBox.style.transform = `translateY(0px)`;
+            arrowIcon.style.transform = `rotate(0deg)`;
+        }    
+    }
+
     return (
-        <section className="select-point-on-map-box graphic graphic">
+        <section className="select-point-on-map-box graphic">
+            <div className="select-point-on-map-box__updown" onClick={getDownTheArea.bind(this)}>
+                <span className="select-point-on-map-box__updowntext">{state.mapBoxTitle}</span>
+                <img src={arrowup} alt="" className="select-point-on-map-box__updownicon"/>
+            </div>
             <h2 className="select-point-on-map-box__title graphic__title text-center">Selecciona la opción que necesites</h2>
             <div className="select-point-on-map-box__grid">
                 <SelectPointOnMap image={ucibed} title="Camas UCI" />
--- src/components/Pages/EmergencyMap.jsx
+++ src/components/Pages/EmergencyMap.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import GoogleMapReact from 'google-map-react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import SelectPointOnMapBox from '../Molecules/SelectPointOnMapBox';
 import SearchRegion from '../Molecules/SearchRegion';
-import InformationPlaceBox from '../Organisms/InformationPlaceBox';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import InformationPlaceBox from '../Templates/InformationPlaceBox';
 
 class EmergencyMap extends React.Component {
-
     static defaultProps = {
         center: {
           lat: 59.95,
@@ -17,30 +15,32 @@ class EmergencyMap extends React.Component {
     };
     
     render() {
-
-        const containerStyle = {
-            position: 'static',  
-            width: '100%',
-            height: '100%'
-        }
+        
+        const Map = ReactMapboxGl({
+            accessToken:
+                'pk.eyJ1IjoicXVpc3Bld2lsbWVyIiwiYSI6ImNrbm8weG1sNzEzZ3cydm1vcWluczVoZHMifQ.51O5VoB0t1sOhThd743wZw'
+        });
 
         return (
             <section className="emergency-map-container">
-                <div style={{ height: '100vh', width: '100%'}} className="emergency-map-container__map-container">
-                    <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyA0WvRYjJMt4RyfXhYbQTF7oPy5_suDoOA"}}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
+                <Map
+                    style="mapbox://styles/mapbox/streets-v9"
+                    containerStyle={{
+                        height: '100%',
+                        width: '100%',
+                        position: 'relative',
+                        gridColumnStart: '1',
+                        gridColumnEnd: '2',
+                        gridRowStart: '1',
+                        gridRowEnd: '4'
+                    }}
                     >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Marker"
-                    />
-                    </GoogleMapReact>
-                </div>
+                    <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+                    </Layer>
+                </Map>
                 <SearchRegion theme="emergency-map-container__search-region" text="Busca tu distrito"/>
-                <InformationPlaceBox />
+                <InformationPlaceBox title="Balones de oxígeno"/>
                 <SelectPointOnMapBox theme="emergency-map-container__select-point-on-map-box"/>
             </section>
         );
