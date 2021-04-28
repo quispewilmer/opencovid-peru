33a28176aecfe29bc2e6b648a21a92e670f84a16
quispewilmer <wilmerquispecode@gmail.com>
2021-04-27 19:10:26 -0500

    Added the district pandemic situation and header links

--- src/components/Organisms/Header.jsx
+++ src/components/Organisms/Header.jsx
@@ -5,113 +5,125 @@ import camasuci from '../../img/header/camas-uci.svg';
 import contagiodistrital from '../../img/header/contagio-distrital.svg';
 import peru from '../../img/header/peru.svg';
 import puntosoxigeno from '../../img/header/puntos-oxigeno.svg';
-import {Link} from 'react-router-dom';
-import {Navbar,Nav,NavDropdown} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
-const Header = () => {
-    
+const Header = ({setCoverActive}) => {
+
     const [isActive, setActive] = useState("false");
 
     const handleToggle = () => {
         setActive(!isActive);
+        setCoverActive();
     };
 
-    return (        
+    return (
         <Navbar bg="white" expand="lg" className="main-header">
-                <Navbar.Brand
-                    as={Link}
-                    to="/"
+            <Navbar.Brand
+                as={Link}
+                to="/"
+            >
+                <img
+                    src={opencovid}
+                    alt="Logo de Open Covid Perú"
+                    className="header__logo-img"
+                    height="50"
+                />
+
+            </Navbar.Brand>
+            <span className={`navtoggle ${isActive ? null : "active"}`}
+                onClick={() => {
+                    handleToggle();
+                }
+                }
+            >
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+            <Nav className={`ml-auto ${isActive ? null : "active"}`} id="sidebar">
+                <NavDropdown
+                    title="Mapa de recursos"
                 >
-                    <img 
-                        src={opencovid} 
-                        alt="Logo de Open Covid Perú" 
-                        className="header__logo-img" 
-                        height="50"
-                    />
-                    
-                </Navbar.Brand>
-                <span className={`navtoggle ${isActive ? null : "active"}`}
-                    onClick={() => {
-                        handleToggle();          
-                        }
-                    }
-                >
-                    <i></i>
-                    <i></i>
-                    <i></i>
-                </span>
-                <Nav className={`ml-auto ${isActive ? null : "active"}`} id="sidebar">                    
-                    <NavDropdown                         
-                        title="Mapa de recursos" 
-                    >
-                        <div className="container-fluid mx-auto text-center  row">
-                            <div className="col">
-                                <img 
-                                    src={camasuci} 
+                    <div className="container-fluid mx-auto text-center  row">
+                        <div className="col">
+                            <Link to="/mapa-emergencia">
+                                <img
+                                    src={camasuci}
                                     alt=""
                                 />
                                 Camas UCI
-                            </div>
-                            <div className="col">
-                                <img 
-                                    src={camascovid} 
+                                </Link>
+                        </div>
+                        <div className="col">
+                            <Link to="/mapa-emergencia">
+                                <img
+                                    src={camascovid}
                                     alt=""
                                 />
-                                Camas COVID</div>
-                            <div className="col">
-                                <img 
-                                    src={puntosoxigeno} 
+                                Camas COVID
+                            </Link>
+                        </div>
+                        <div className="col">
+                            <Link to="/mapa-emergencia">
+                                <img
+                                    src={puntosoxigeno}
                                     alt=""
                                 />
                                 Puntos de Oxígeno
-                            </div>
-                        </div> 
-                    </NavDropdown>
-                    <NavDropdown                         
-                        title="Estado de pandemia" 
-                    >
-                        <div className="triangulo-equilatero-bottom"></div>
-                        <div className="container-fluid mx-auto text-center  row">
-                            <div className="col">
-                                <img 
-                                    src={peru} 
+                                </Link>
+                        </div>
+                    </div>
+                </NavDropdown>
+                <NavDropdown
+                    title="Estado de pandemia"
+                >
+                    <div className="triangulo-equilatero-bottom"></div>
+                    <div className="container-fluid mx-auto text-center  row">
+                        <div className="col">
+                            <Link to="estado-pandemia">
+                                <img
+                                    src={peru}
                                     alt=""
                                 />
                                 Situacion regional
-                            </div>
-                            <div className="col">
-                                <img 
-                                    src={contagiodistrital} 
+                            </Link>
+                        </div>
+                        <div className="col">
+                            <Link to="situacion-distrital-pandemia">
+                                <img
+                                    src={contagiodistrital}
                                     alt=""
                                 />
                                 Contagios distritales
-                            </div>
-                        </div> 
-                    </NavDropdown>
-                    <Nav.Link 
-                        as={Link} 
-                        to="/vacunacion" 
-                        exact="true"
-                    >
-                        Vacunación
+                                </Link>
+                        </div>
+                    </div>
+                </NavDropdown>
+                <Nav.Link
+                    as={Link}
+                    to="/vacunacion"
+                    exact="true"
+                >
+                    Vacunación
                     </Nav.Link>
-                    <Nav.Link
-                        as={Link} 
-                        to="/sobre-covid" 
-                        exact="true"
-                    >
-                        Sobre COVID
+                <Nav.Link
+                    as={Link}
+                    to="/sobre-covid"
+                    exact="true"
+                >
+                    Sobre COVID
                     </Nav.Link>
-                    <Nav.Link 
-                        as={Link} 
-                        to="/nosotros" 
-                        exact="true"
-                    >
-                        Nosotros
+                <Nav.Link
+                    as={Link}
+                    to="/nosotros"
+                    exact="true"
+                >
+                    Nosotros
                     </Nav.Link>
-                </Nav>
-            </Navbar>
-            
+            </Nav>
+        </Navbar>
+
     )
 }
 
