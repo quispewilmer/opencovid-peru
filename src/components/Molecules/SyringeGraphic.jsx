08a1dc5f50f795d87b75fde6c864e68156a37084
Sandro Jurado <sj@sandrojurado.com>
2021-05-02 19:36:52 -0500

    + Bloque de fecha de fin de vacunación  + Bloque de avance de vacunación

---
+++ src/components/Molecules/SyringeGraphic.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Chart, Bar } from 'react-chartjs-2';
+import SyringeIcon from '../../img/vaccination/syringe.svg';
+import CounterIcon from '../../img/vaccination/counter.svg';
+import CalendarIcon from '../../img/vaccination/calendar.svg';
+import SyringeBg from '../../img/vaccination/syringe-bg.svg';
+var moment = require('moment/min/moment-with-locales');
+moment.locale('es');
+
+const SyringeGraphic = (props) => {
+
+    let graphData = [];
+    graphData = (props.data !== null ) ? props.data.data || []  : null;
+
+    console.log(graphData)
+
+
+    return (
+        <section className="graph-block">
+            <h4 className={'text-justify'}>
+                Entonces, a este ritmo, ¿en cuánto tiempo se vacunarían todas las personas mayores de edad en el Perú?
+            </h4>
+            <div className="row">
+                <div className="col-12">
+                    <div className="row">
+                        <div className="col-9 align-items-center justify-content-center d-flex">
+                            <div className={'vaccine-progress'}>
+
+                                <div style={{ marginTop: '5%', marginLeft: '13%', width: '65%', height: '46%'}} className={'align-items-end justify-content-end d-flex'}>
+
+                                    <div style={{ width: Math.round(graphData.vaccProgress) + '%', height: '100%', backgroundColor: 'rgba(1,171,246,0.5)'}}>
+                                        <span style={{ position: 'relative', top: '-70%', left: 0 }}>{graphData.vaccProgress}%</span>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <img src={SyringeBg}  className="img-fluid d-inline-block" />
+                        </div>
+                        <div className="col-3">
+                            <img src={CalendarIcon}  className="img-fluid d-inline-block" />
+                            <div className={'vaccine-calendar align-items-center justify-content-center d-flex'}>
+                                El {moment(graphData.vaccEnds, 'DD-MM-YYYY HH:mm:ss').format('DD/MM/YYYY')}<br/>
+                                {moment(graphData.vaccEnds, 'DD-MM-YYYY HH:mm:ss').diff(moment(),'months')} meses
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p className="graph-block__source">Fuente: Gobierno del Perú 2021. Coronavirus: vacunas contra la COVID-19 en el Perú</p>
+        </section>
+    );
+}
+
+export default SyringeGraphic;
---
+++ src/components/Molecules/VaccinesGraphic.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { Chart, Bar } from 'react-chartjs-2';
+import SyringeIcon from '../../img/vaccination/syringe.svg';
+import CounterIcon from '../../img/vaccination/counter.svg';
+import DailyIcon from '../../img/vaccination/daily.svg';
+var moment = require('moment/min/moment-with-locales');
+moment.locale('es');
+
+const VaccinesGraphic = (props) => {
+
+    let graphData = [];
+    graphData = props.data.data || [];
+
+    const data = {
+        labels: graphData.map((item) => {
+            return moment(item.fecha, 'DD-MM-YYYY HH:mm:ss').format('D MMM');
+        }),
+        datasets: [
+            {
+                label: 'Primeras dosis aplicadas',
+                data: graphData.map((item) => {
+                     return item.diario;
+                    }),
+                backgroundColor: [
+                    '#01ABF6'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: 'Número de vacunas diarias a aplicar para llegar a la meta',
+                data: graphData.map((item) => {
+                    return item.meta;
+                }),
+                backgroundColor: [
+                    '#00599D'
+                ],
+                borderColor: [
+                    '#00599D',
+                ],
+                type: 'line',
+                borderWidth: 3,
+            },
+        ],
+    };
+
+    Chart.defaults.font.size = 16;
+
+    const options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    };
+
+    return (
+        <section className="graph-block">
+            {graphData.length > 0 && (
+                <h4 className={'text-center'}>
+                    Al 31 de diciembre, se deben vacunar a <i className="graph-block__amount">{(graphData[0].resta / 1000000).toFixed(2)}</i> millones de personas en el Perú.
+                </h4>
+            )}
+            <div className="row">
+                <div className="col-4">
+                    <div className="row justify-content-center align-items-center flex-column">
+                        <div className="text-center d-block h-100px">
+                            <img src={SyringeIcon}  className="img-fluid d-inline-block" />
+                        </div>
+                        {graphData.length > 0 && (
+                            <div className="graph-block__textbox pt-1 pb-1 pl-3 pr-3 mt-3 mb-3 fs-20">
+                                {graphData[graphData.length - 1].resta.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+                            </div>
+                        )}
+                        <div className="d-block text-center">
+                            <p className="graph-block__boxlabel p-0 m-0">Total vacunas que<br/>faltan aplicar</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="row justify-content-center align-items-center flex-column">
+                        <div className="text-center h-100px align-items-center pt-3">
+                            <img src={CounterIcon}  className="img-fluid d-inline-block" />
+                        </div>
+                        <div className="graph-block__textbox pt- pb-1 pl-3 pr-3 mt-3 mb-3 fs-20">
+                            {moment('2021-12-31').diff(moment(),'days')}
+                        </div>
+                        <div className="d-block text-center">
+                            <p className="graph-block__boxlabel p-0 m-0">Días para el 31<br/>de diciembre</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="row justify-content-center align-items-center flex-column">
+                        <div className="text-center d-block h-100px">
+                            <img src={DailyIcon}  className="img-fluid d-inline-block" />
+                        </div>
+                        {graphData.length > 0 && (
+                            <div className="graph-block__textbox pt-1 pb-1 pl-3 pr-3 mt-3 mb-3 fs-20">
+                                {Math.round(graphData[graphData.length - 1].meta)}
+                            </div>
+                        )}
+                        <div className="d-block text-center">
+                            <p className="graph-block__boxlabel p-0 m-0">Vacunas se deben<br/>aplicar por día</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="p-3">
+                <Bar data={data} options={options} width={'100%'}
+                     height={550}
+                     options={{ maintainAspectRatio: false }} />
+            </div>
+
+            <p className="graph-block__source">Fuente: Gobierno del Perú 2021. Coronavirus: vacunas contra la COVID-19 en el Perú</p>
+        </section>
+    );
+}
+
+export default VaccinesGraphic;
