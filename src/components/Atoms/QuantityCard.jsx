fd276e51c8875c547fb7847e77b6e068d371d434
Alexander Valerio Cruz <ralexandervc@gmail.com>
2021-05-16 21:45:05 -0500

    changes alexander

--- src/components/Atoms/QuantityCard.jsx
+++ src/components/Atoms/QuantityCard.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef} from 'react';
+import {NavLink} from 'react-router-dom';
 
-const QuantityCard = ({ title, image, number, type, cardMedidPerson, update }) => {
+const QuantityCard = ({ title, image, number, type, cardMedidPerson, update ,link}) => {
 
     const numberRef = useRef(null);
 
@@ -17,18 +18,18 @@ const QuantityCard = ({ title, image, number, type, cardMedidPerson, update }) =
     })
 
     return (
-        <button className={`quantity-card ${cardMedidPerson} d-flex align-items-center`}>
+        <NavLink className={`quantity-card ${cardMedidPerson} d-flex align-items-center`} to={link}>
             <div className="d-flex align-items-center px-0 w-100">
                 <div className="quantity-card__image px-0 col-3">
                     <img src={image} alt={title} />
                 </div>
                 <div className="quantity-card__content px-1 text-center col-9">
-                    <p className={`quantity-card__title mb-0 d-flex justify-content-center`}>{title}</p>
+                    <p className={`quantity-card__title mb-0 d-flex justify-content-center text-dark`}>{title}</p>
                     <p className={`quantity-card__number quantity-${type} mt-0 font-weight-bold`} ref={numberRef}>{number}</p>
                     <p className="quantity-card__update">{`Actualizado: ${update}`}</p>
                 </div>
             </div>
-        </button>
+        </NavLink>
     )
 }
 
