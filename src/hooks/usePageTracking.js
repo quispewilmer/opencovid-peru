8c45c431d66c3477a44be0a9e8b9a19aafe814a4
Ragi Yaser Burhum <ragi@amigocloud.com>
2021-05-19 19:20:47 -0500

    Remove console.log entries

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -5,21 +5,18 @@ import ReactGA from "react-ga";
 const usePageTracking = () => {
   const location = useLocation();
   const [initialized, setInitialized] = useState(false);
-
-  console.log(location);
+  
   useEffect(() => {
-    //if (!window.location.href.includes("localhost222")) {
-      ReactGA.initialize("UA-179517549-1", { debug: true });
-    //}
+    if (!window.location.href.includes("localhost")) {
+      ReactGA.initialize("UA-179517549-1");
+    }
     setInitialized(true);
   }, []);
 
   useEffect(() => {
     if (initialized) {
-      ReactGA.pageview(location.pathname + location.search);
-      console.log('pageview');
-    }
-    console.log('out of location: ', location);
+      ReactGA.pageview(location.pathname + location.search);      
+    }    
   }, [initialized, location]);
 };
 
